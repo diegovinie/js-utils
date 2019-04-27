84dc58d03e2c401fba9beaa56276635fbe81c9ff
Diego Viniegra <diego.viniegra@gmail.com>
2019-04-27 14:53:55 -0500

    Write index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// TODO
+import * as general from './general'
+import DragAndDrop from './DragAndDrop'
+import IframeKeyboardEvents from './IframeKeyboardEvents'
+
+export default {
+  general,
+  DragAndDrop,
+  IframeKeyboardEvents
+}
