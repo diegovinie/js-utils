624f22936d1a8219b2f60c6b0e32891fd87b060b
Diego Viniegra <diego.viniegra@gmail.com>
2019-04-28 09:49:18 -0500

    Install babel & jest

---
+++ test/unit/jest.conf.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: [
+    'js'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
+  },
+  coverageDirectory: '<rootDir>/test/unit/coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!**/node_modules/**'
+  ]
+}
